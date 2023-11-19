bb9b7d9e15f1911b58b15a06362d86d41fe42e9c
kevin <kevin1025253@gmail.com>
2023-11-19 23:53:55 +0800

    add:连接mysql

--- db/index.js
+++ db/index.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql')
-const db = mysql.createPool({
+const db = mysql.createConnection({
     host: '127.0.0.1',
     user :'root',
     password: '123456',
     socketPath: '/tmp/mysql.sock',
-    database: "mysql",
+    database: "test",
 })
-
-db.query("show tables",(err,result) => {
+db.connect()
+const sql = "SELECT * FROM kevin"
+// 查询
+// 菜鸟文档：https://www.runoob.com/nodejs/nodejs-mysql.html
+db.query(sql,(err,result) => {
     if(err) return console.log('err info', err)
     console.log('result',result)
 })
+db.end()
