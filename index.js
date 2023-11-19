5c28dd504ccdf59c86c7ada89ea46593766b767a
kevin <kevin1025253@gmail.com>
2023-11-19 17:25:47 +0800

    add:express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// 路由
+const home = require('./routes/home');
+
+app.get('/',(req,res) => {
+    res.send('hello world')
+})
+
+app.listen(port,() => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.use('/home',home)
