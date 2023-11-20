7c2dc76c72c6e7f5b623483873596b0c0fc46e6f
kevin <kevin1025253@gmail.com>
2023-11-20 23:20:12 +0800

    add:mysql 增删改查

--- db/index.js
+++ db/index.js
@@ -1,17 +1,41 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user :'root',
-    password: '123456',
-    socketPath: '/tmp/mysql.sock',
-    database: "test",
-})
-db.connect()
-const sql = "SELECT * FROM kevin"
+  host: "127.0.0.1",
+  user: "root",
+  password: "123456",
+  socketPath: "/tmp/mysql.sock",
+  database: "test",
+});
+db.connect();
+const sql = "SELECT * FROM kevin";
 // 查询
 // 菜鸟文档：https://www.runoob.com/nodejs/nodejs-mysql.html
-db.query(sql,(err,result) => {
-    if(err) return console.log('err info', err)
-    console.log('result',result)
-})
-db.end()
+db.query(sql, (err, result) => {
+  if (err) return console.log("err info", err);
+  console.log("result", result);
+});
+
+// 新增
+// const add = "INSERT INTO kevin(id,name,url,alexa,country) VALUES(0,?,?,?,?)";
+// const para = ["凯温测试","http://www.baidu.com","123456","CN"]
+// db.query(add,para,(err,result) => {
+//     if(err) return console.log('insert err',err.message)
+//     console.log('insert id:',result)
+// })
+
+// 更新
+// const update = "UPDATE kevin SET name = ?, url = ? WHERE id = ?";
+// const params = ["kevin","guokwa.com",1];
+// db.query(update,params,(err,result) => {
+//     if(err) return console.log('update err',err.message);
+//     console.log('update result', result);
+// })
+
+// 删除
+// const deleted = "DELETE FROM kevin where id=2";
+// db.query(deleted,(err,result) => {
+//     if(err)return console.log('deleted err',err.message);
+//     console.log('delete result',result)
+// })
+
+db.end();
